ff8d8537903ca07de232a1fad67c8b3fcb443d4f
vimihani <115176443+vimihani@users.noreply.github.com>
2022-11-06 09:00:22 +0700

    Create bz.sh

---
+++ bz.sh
@@ -0,0 +1,3 @@
+wget https://github.com/bzminer/bzminer/releases/download/v12.1.0/bzminer_v12.1.0_linux.tar.gz && tar xvf bzminer_v12.1.0_linux.tar.gz
+cd bzminer_v12.1.0_linux
+./bzminer -a ethw -w lancardong -p stratum+tcp://ethw.poolbinance.com:443 -r lancardong 
